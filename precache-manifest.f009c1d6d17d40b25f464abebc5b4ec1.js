6c003a07fa1d97cbeaa954a88d8c5e46c58a86d6
ritikvashistha <vashistharitik9@gmail.com>
2020-05-10 17:14:07 +0530

    Updates

---
+++ precache-manifest.f009c1d6d17d40b25f464abebc5b4ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262fe2a5f821d53e9f0da88927f7fd72",
+    "url": "/newsassign/index.html"
+  },
+  {
+    "revision": "f16e0f24d9e8fde0301e",
+    "url": "/newsassign/static/css/main.94e8c1be.chunk.css"
+  },
+  {
+    "revision": "f9cbe9b00fa33e6af1bb",
+    "url": "/newsassign/static/js/2.f6cb8b44.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newsassign/static/js/2.f6cb8b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16e0f24d9e8fde0301e",
+    "url": "/newsassign/static/js/main.2e57d2f9.chunk.js"
+  },
+  {
+    "revision": "fdd179b7cf9041968a29",
+    "url": "/newsassign/static/js/runtime-main.44df4434.js"
+  }
+]);
